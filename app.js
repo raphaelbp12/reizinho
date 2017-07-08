849b84c4b21c4ab6b6c0d40bdf61886d3353c94b
raphaelbp12 <raphaelbp12@gmail.com>
2017-07-08 00:17:34 -0300

    Tentativa de fazer o GET

--- app.js
+++ app.js
@@ -3,7 +3,26 @@
  */
 var App = angular.module('App', ['restangular']);
 
-App.controller('indexController', function($scope, Restangular){
-    $scope.teste = 'teste';
-});
+App.controller('indexController', ['$scope', 'DataService', function($scope, DataService){
+    DataService.getData();
 
+    $scope.teste = 'teste';
+}]);
+
+angular.module('App').factory('DataRest', ['$q', 'Restangular', function ($q, Restangular) {
+    return Restangular.withConfig(function(RestangularConfigurer) {
+        RestangularConfigurer.setBaseUrl('http://34.229.154.187/');
+    });
+}]);
+
+angular.module('App').service('DataService', ['DataRest', function (DataRest) {
+
+    var self = this;
+
+    self.getData = function () {
+        //console.log("twitch called");
+        DataRest.all('data').getList().then(function(response) {
+            console.log("response", response);
+        });
+    };
+}]);
