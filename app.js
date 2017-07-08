2360a4e02a0c49516b3addd46b942e9bc3664402
raphaelbp12 <raphaelbp12@gmail.com>
2017-07-07 23:44:24 -0300

    Remove the now ignored directory node_modules

--- app.js
+++ app.js
@@ -1,3 +1,9 @@
 /**
  * Created by raphaelbp12 on 7/7/2017.
  */
+var App = angular.module('App', ['restangular']);
+
+App.controller('indexController', function($scope, Restangular){
+    $scope.teste = 'teste';
+});
+
