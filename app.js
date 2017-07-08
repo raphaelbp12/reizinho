6350f7644c3426bfc78c444bdc4b28ce7723d699
raphaelbp12 <raphaelbp12@gmail.com>
2017-07-08 00:51:35 -0300

    Ajax funcionando

--- app.js
+++ app.js
@@ -4,25 +4,24 @@
 var App = angular.module('App', ['restangular']);
 
 App.controller('indexController', ['$scope', 'DataService', function($scope, DataService){
+
     DataService.getData();
 
     $scope.teste = 'teste';
 }]);
 
-angular.module('App').factory('DataRest', ['$q', 'Restangular', function ($q, Restangular) {
-    return Restangular.withConfig(function(RestangularConfigurer) {
-        RestangularConfigurer.setBaseUrl('http://34.229.154.187/');
-    });
-}]);
-
-angular.module('App').service('DataService', ['DataRest', function (DataRest) {
+angular.module('App').service('DataService', ['$http', function ($http) {
 
     var self = this;
 
     self.getData = function () {
         //console.log("twitch called");
-        DataRest.all('data').getList().then(function(response) {
-            console.log("response", response);
+        $http({
+            method: "GET",
+            url: "http://34.229.92.175/data/",
+            headers: {'Content-Type': 'application/json;'}
+        }).then(function(restsData) {
+            console.log("response", restsData.data.data);
         });
     };
 }]);
