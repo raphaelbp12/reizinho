945ac43ae9548cd80c6b8bf123628ec38dbdc728
Caio César Riqueza Ramos <caiotecn@yahoo.com.br>
2017-07-10 00:07:31 -0300

    Personalização seção Quem somos e base para as galerias

--- Directives/nossasBandas.js
+++ Directives/nossasBandas.js
@@ -7,6 +7,15 @@ angular.module('App').directive("nossasBandas", ['DataService', 'Common' , funct
             $scope.galleries_nossasBandas = [];
             Common.calcGalleries($scope.data.galerias, $scope.galleries_nossasBandas);
 
+
+            $scope.gallerySelected = $scope.galleries_nossasBandas[0];
+
+            $scope.changeGallery = function (id){
+              if ($scope.galleries_nossasBandas[id].id != $scope.gallerySelected.id){
+                $scope.gallerySelected = $scope.galleries_nossasBandas[id];
+              }
+            }
+
         }],
         templateUrl : 'Sections/nossasBandas.html'
     };
--- Services/Common.js
+++ Services/Common.js
@@ -9,33 +9,35 @@ angular.module('App').service('Common', ['DataService', function (DataService) {
     self.calcGalleries = function(images,galleries){
         images.forEach(function (image, indexImage) {
             var foundGallery = false;
+            var cover = image.alttext === self.COVER_STRING;
+            var img = {
+                gallery: image.title,
+                url: self.calcURL(image.path, image.filename),
+                description: image.description,
+                alttext: image.alttext
+            }
+
             galleries.forEach(function (gallery, indexGallery) {
                 if(gallery.name === image.title){
                     foundGallery = true;
-                    var img = {
-                        gallery: image.title,
-                        url: self.calcURL(image.path, image.filename),
-                        description: image.description,
-                        alttext: image.alttext
-                    }
-                    if (image.alttext == self.COVER_STRING){
+                    if (cover){
                       galleries[indexGallery].cover = img;
                     } else {
                       galleries[indexGallery].images.push(img);
                     }
                 }
+
             });
             if(!foundGallery){
                 galleries.push({
+                    id: galleries.length,
                     name: image.title,
-                    images: [{
-                        gallery: image.title,
-                        url: self.calcURL(image.path, image.filename),
-                        description: image.description,
-                        alttext: image.alttext
-                    }]
+                    images: cover ? [img] : [],
+                    cover: cover? img : null
                 });
+
             }
+
         })
 
         console.log("$scope.galleries", galleries);
