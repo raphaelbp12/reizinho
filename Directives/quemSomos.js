5acd327bcccf5ac9085646fdaa1b3bb9febc32bf
raphaelbp12 <raphaelbp12@gmail.com>
2017-08-02 20:00:53 -0300

    Botoes do quem somos

--- Directives/quemSomos.js
+++ Directives/quemSomos.js
@@ -4,6 +4,21 @@ angular.module('App').directive("quemSomos", ['DataService', function(DataServic
             data: '='
         },
         controller: ['$scope', 'DataService', function QuemSomosController($scope, DataService) {
+            $scope.missaoCtrl = false;
+            $scope.valoresCtrl = false;
+            $scope.visaoCtrl = false;
+
+            $scope.toggleMissao = function () {
+                $scope.missaoCtrl = !$scope.missaoCtrl;
+            };
+
+            $scope.toggleValores = function () {
+                $scope.valoresCtrl = !$scope.valoresCtrl;
+            };
+
+            $scope.toggleVisao = function () {
+                $scope.visaoCtrl = !$scope.visaoCtrl;
+            };
         }],
         templateUrl : 'Sections/quemSomos.html'
     };
