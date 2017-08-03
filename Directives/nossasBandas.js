d4d1721a03841d5f279c39c448276518084d0da5
Caio César Riqueza Ramos <caiotecn@yahoo.com.br>
2017-08-03 00:14:22 -0300

    Slick responsivo; Algumas customizações em nossasBandas

--- Directives/nossasBandas.js
+++ Directives/nossasBandas.js
@@ -44,7 +44,40 @@ angular.module('App').directive("nossasBandas", ['DataService', 'Common', functi
                     "arrows": true,
                     "autoplay": true,
                     "autoplaySpeed": 500,
-                    "dots": true
+                    "dots": true,
+                    "responsive": [
+                      {
+                        breakpoint: 1366,
+                        settings: {
+                          slidesToShow: 4,
+                          slidesToScroll: 4
+                        }
+                      },
+                      {
+                        breakpoint: 1024,
+                        settings: {
+                          slidesToShow: 3,
+                          slidesToScroll: 3
+                        }
+                      },
+                      {
+                        breakpoint: 600,
+                        settings: {
+                          slidesToShow: 2,
+                          slidesToScroll: 2
+                        }
+                      },
+                      {
+                        breakpoint: 480,
+                        settings: {
+                          slidesToShow: 1,
+                          slidesToScroll: 1
+                        }
+                      }
+                      // You can unslick at a given breakpoint now by adding:
+                      // settings: "unslick"
+                      // instead of a settings object
+                    ]
                 });
 
                 $scope.changeGallery(0);
