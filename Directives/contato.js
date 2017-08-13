2ce86a0a50e758e664a073a83fefdcd4ce165149
raphaelbp12 <raphaelbp12@gmail.com>
2017-08-12 23:08:30 -0300

    introducao do google maps e correcao na funcao enviar dos contatos

--- Directives/contato.js
+++ Directives/contato.js
@@ -1,43 +1,48 @@
-angular.module('App').directive("contato", ['DataService', function(DataService) {
+angular.module('App').directive("contato", ['DataService', 'NgMap', function (DataService, NgMap) {
     return {
         scope: {
             data: '=',
             general: '='
         },
-        controller: ['$scope', 'DataService', function ContatoController($scope, DataService)  {
-          $scope.data.tipo = ["CASAMENTO", "FORMATURA", "ANIVERSÁRIO", "CORPORATIVO", "WORKSHOP"];
-          $scope.data.precisa = ["BANDA BAILE","DJ", "ORQUESTRA PARA CERIMONIAL","ORQUESTRA PARA RECEPTIVO",
-                                  "BOATE","ESTRUTURAS PARA PALCO","ILUMINAÇÃO CÊNICA","SONORIZAÇÃO",
-                                  "PAINÉIS LED","EFEITOS ESPECIAIS"];
-          $scope.email = {};
-          $scope.email.tipo = $scope.data.tipo[0];
-
-          $scope.showForm = true;
-          $scope.enviarButton = true;
-          $scope.showError = false;
-          $scope.showSuccess = false;
-          $scope.showLoading = false;
-
-          $scope.enviar = function () {
-            $scope.showLoading = true;
-            $scope.enviarButton = false;
-            DataService.sendEmail($scope.email).then(() => {
-              //sucess
-              $scope.showForm = $scope.showLoading = false;
-              $scope.showSuccess = true;
-            },
-          () => {
-            //fail
-            $scope.showForm = $scope.showLoading = false;
-            $scope.showError = true;
-          });
-
-          }
-          $scope.limpar = function () {
+        controller: ['$scope', 'DataService', 'NgMap', function ContatoController($scope, DataService, NgMap) {
+            $scope.data.tipo = ["CASAMENTO", "FORMATURA", "ANIVERSÁRIO", "CORPORATIVO", "WORKSHOP"];
+            $scope.data.precisa = ["BANDA BAILE", "DJ", "ORQUESTRA PARA CERIMONIAL", "ORQUESTRA PARA RECEPTIVO",
+                "BOATE", "ESTRUTURAS PARA PALCO", "ILUMINAÇÃO CÊNICA", "SONORIZAÇÃO",
+                "PAINÉIS LED", "EFEITOS ESPECIAIS"];
             $scope.email = {};
             $scope.email.tipo = $scope.data.tipo[0];
-          }
-        }],
-        templateUrl : 'Sections/contato.html'
-    };
+
+            $scope.showForm = true;
+            $scope.enviarButton = true;
+            $scope.showError = false;
+            $scope.showSuccess = false;
+            $scope.showLoading = false;
+
+            NgMap.getMap().then(function (map) {
+                console.log(map.getCenter());
+                console.log('markers', map.markers);
+                console.log('shapes', map.shapes);
+            });
+
+            $scope.enviar = function () {
+                $scope.showLoading = true;
+                $scope.enviarButton = false;
+                DataService.sendEmail($scope.email).then(function () {
+                    $scope.showForm = $scope.showLoading = false;
+                    $scope.showSuccess = true;
+
+                }, function () {
+                    //fail
+                    $scope.showForm = $scope.showLoading = false;
+                    $scope.showError = true;
+                })};
+
+                $scope.limpar = function () {
+                    $scope.email = {};
+                    $scope.email.tipo = $scope.data.tipo[0];
+                };
+            }
+        ],
+            templateUrl: 'Sections/contato.html'
+        };
 }]);
