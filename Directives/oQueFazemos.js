8397348280ff1676ada0870efe5ad1080a74945d
Caio César Riqueza Ramos <caiotecn@yahoo.com.br>
2017-08-12 22:29:11 -0300

    Seção o que fazemos *mudança no tratamento de cover da galeria

--- Directives/oQueFazemos.js
+++ Directives/oQueFazemos.js
@@ -1,24 +1,101 @@
-angular.module('App').directive("oQueFazemos", ['DataService', 'Common', function(DataService, Common) {
+angular.module('App').directive("oQueFazemos", ['DataService', 'Common', '$timeout', function(DataService, Common, $timeout) {
     return {
         scope: {
-            data: '='
+            data: '=',
+            general: '='
         },
         controller: ['$scope','DataService','Common', function OQueFazemosController($scope, DataService, Common) {
           $scope.galleries_oQueFazemos = [];
           Common.calcGalleries($scope.data.galerias, $scope.galleries_oQueFazemos);
 
           $scope.gallerySelected = $scope.galleries_oQueFazemos[0];
+          console.log($scope.gallerySelected);
+
+          $scope.slickGaleriasConfig = {
+                    "slidesToShow": 7,
+                    "slidesToScroll": 7,
+                    "arrows": true,
+                    "autoplay": false,
+                    "dots": true,
+                    responsive: [
+                      {
+                        breakpoint: 600,
+                        settings: {
+                          slidesToShow: 4,
+                          slidesToScroll: 4
+                        }
+                      },
+                      {
+                        breakpoint: 480,
+                        settings: {
+                          slidesToShow: 3,
+                          slidesToScroll: 3
+                        }
+                      }
+                    ],
+              method: {},
+              event: {
+                  beforeChange: function (event, slick, currentSlide, nextSlide) {
+                  },
+                  afterChange: function (event, slick, currentSlide, nextSlide) {
+                  }
+              }
+          };
+
+          $scope.slickFazemosConfig = {
+                    "slidesToShow": 3,
+                    "slidesToScroll": 3,
+                    "arrows": true,
+                    "autoplay": false,
+                    "dots": true,
+                    responsive: [
+                      {
+                        breakpoint: 600,
+                        settings: {
+                          slidesToShow: 2,
+                          slidesToScroll: 2
+                        }
+                      },
+                      {
+                        breakpoint: 480,
+                        settings: {
+                          slidesToShow: 1,
+                          slidesToScroll: 1
+                        }
+                      }
+                    ],
+              method: {},
+              event: {
+                  beforeChange: function (event, slick, currentSlide, nextSlide) {
+                  },
+                  afterChange: function (event, slick, currentSlide, nextSlide) {
+                  }
+              }
+          };
 
           $scope.changeGallery = function (id) {
-                  $scope.gallerySelected.images.forEach(function (image, imageIndex) {
-                      // $('#galeria-oQueFazemos').slick('slickRemove',0);
+                  // $scope.gallerySelected.images.forEach(function (image, imageIndex) {
+                  //     // $('#galeria-oQueFazemos').slick('slickRemove',0);
+                  // });
+                  //
+                  // $scope.gallerySelected = $scope.galleries_oQueFazemos[id];
+                  //
+                  // $scope.gallerySelected.images.forEach(function (image, imageIndex) {
+                  //     // $('#galeria-oQueFazemos').slick('slickAdd',"<div  class='oQueFazemos-gallery-item' style='background-image: url("+image.url_thumb+")'></div>");
+                  // });
+
+                  $scope.galleries_oQueFazemos.forEach(function (gallery, galleryIndex) {
+                    $('#container-galerias-fazemos #'+gallery.id).removeClass('gallery-selected');
                   });
 
-                  $scope.gallerySelected = $scope.galleries_oQueFazemos[id];
+                  console.log(id)
+                  $scope.arrayLoaded = false;
 
-                  $scope.gallerySelected.images.forEach(function (image, imageIndex) {
-                      // $('#galeria-oQueFazemos').slick('slickAdd',"<div  class='oQueFazemos-gallery-item' style='background-image: url("+image.url_thumb+")'></div>");
-                  });
+                  $('#container-galerias-fazemos #'+id).addClass('gallery-selected');
+                  $scope.gallerySelected = $scope.galleries_oQueFazemos[id] ;
+                  $timeout(function(){
+                   $scope.arrayLoaded = true;
+                 },500);
           };
 
 
--- Services/Common.js
+++ Services/Common.js
@@ -1,7 +1,6 @@
 angular.module('App').service('Common', ['DataService', function (DataService) {
 
     var self = this;
-    self.COVER_STRING = '#img_cover_gallery#';
 
     self.modalAlreadyOpened = false;
     self.modalSelectedGallery = {};
@@ -24,10 +23,10 @@ angular.module('App').service('Common', ['DataService', function (DataService) {
         return DataService.WPURL + path + "/" + filename;
     };
 
-    self.calcGalleries = function(images,galleries, videos, gallery_videos){
+    self.calcGalleries = function(images,galleries){
         images.forEach(function (image, indexImage) {
             var foundGallery = false;
-            var cover = image.alttext === self.COVER_STRING;
+            var cover = !!parseInt(image.cover);
             var img = {
                 gallery: image.title,
                 url: self.calcURL(image.path, image.filename),
@@ -35,11 +34,11 @@ angular.module('App').service('Common', ['DataService', function (DataService) {
                 description: image.description,
                 alttext: image.alttext
             }
-
             galleries.forEach(function (gallery, indexGallery) {
                 if(gallery.name === image.title){
                     foundGallery = true;
                     if (cover){
+                      galleries[indexGallery].description = img.description
                       galleries[indexGallery].cover = img;
                     } else {
                       galleries[indexGallery].images.push(img);
@@ -52,7 +51,8 @@ angular.module('App').service('Common', ['DataService', function (DataService) {
                     id: galleries.length,
                     name: image.title,
                     images: cover ? [] : [img],
-                    cover: cover? img : null
+                    cover: cover? img : null,
+                    description: cover ? image.description : ''
                 });
 
             }
