bc182bc9cf6f9d8ea2ab9c6923bbb285c199dc4c
raphaelbp12 <raphaelbp12@gmail.com>
2017-08-14 22:14:16 -0300

    facebook no footer

--- Directives/footer.js
+++ Directives/footer.js
@@ -1,13 +1,12 @@
-angular.module('App').directive("footer", ['DataService', 'Common', function(DataService, Common) {
+angular.module('App').directive("footer", ['DataService', 'Common', function (DataService, Common) {
     return {
         scope: {
             data: '=',
             general: '='
         },
-        controller: ['$scope','DataService','Common', function FooterController($scope, DataService, Common) {
-          $scope.common = Common;
-
+        controller: ['$scope', 'DataService', 'Common', function FooterController($scope, DataService, Common) {
+            $scope.common = Common;
         }],
-        templateUrl : 'Sections/footer.html'
+        templateUrl: 'Sections/footer.html'
     };
 }]);
